b99b8e4d7b3be4475ac5831d5f85789e7cb12de4
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-07-05 17:38:06 +0500

    Add project files.

---
+++ NumbersOrLetters/Program.cs
@@ -0,0 +1,31 @@
+﻿using NumbersOrLetters.Services;
+using System;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        bool isTrue = true;
+        do
+        {
+            try
+            {
+
+                Console.WriteLine("\t\t\t\t\tWelcome!!!\n\tThis program determines the number of numbers, letters and other characters in the entered text");
+                Console.Write("Enter the text => ");
+                string userInput = Console.ReadLine();
+                NumberOrLetter.PrintRequest(userInput);
+
+                Console.WriteLine("Do you want to continue? (yes / no)");
+                string checkWhile = Console.ReadLine().ToLower();
+                isTrue = checkWhile is "yes" or "y";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        } while (isTrue);
+    }
+}
---
+++ NumbersOrLetters/Services/NumberOrLetter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace NumbersOrLetters.Services
+{
+    internal class NumberOrLetter
+    {
+        private static int lettersCount = 0;
+        private static int numbersCount = 0;
+        private static int othersCount = 0;
+
+        private static string letters = "";
+        private static string numbers = "";
+        private static string otherCharacters = "";
+
+
+        private static void CheckInput(string input)
+        {
+            if (input == null)
+            {
+                Console.WriteLine("Input is NULL");
+            }
+            else
+            {
+                foreach (char c in input)
+                {
+                    if (!char.IsLetterOrDigit(c))
+                    {
+                        if (char.IsWhiteSpace(c))
+                        {
+                            otherCharacters += " space ";
+                        }
+                        otherCharacters += c;
+                        othersCount++;
+                    }
+                    else if (char.IsDigit(c))
+                    {
+                        numbers += c;
+                        numbersCount++;
+                    }
+                    else
+                    {
+                        letters += c;
+                        lettersCount++;
+                    }
+                }
+            }
+
+        }
+        public static void PrintRequest(string input)
+        {
+            CheckInput(input);
+
+            Console.WriteLine($"""the number of letters in the text : "{lettersCount}" and letters is => '{letters}' """);
+            Console.WriteLine($"""the number of numbers in the text : "{numbersCount}" and numbers is => '{numbers}' """);
+            Console.WriteLine($"""the number of letters in the text : "{othersCount}" and letters is => '{otherCharacters}' """);
+        }
+    }
+}
